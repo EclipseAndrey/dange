83d6abdc46181a2e364d6e8ed0c6d905aa64e30d
Andrey <a.korenkov@cryptology.com>
2023-05-25 15:17:40 +0300

    web

--- webrunner/flutter_service_worker.js
+++ webrunner/flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "eeb05e2be273fc17137ae205c15d6b0a",
-"index.html": "d67beafca0d5344f911bf0fc98aa5f5f",
-"/": "d67beafca0d5344f911bf0fc98aa5f5f",
+  "version.json": "d4f11a6ce094ee37257169b4a4a69062",
+"index.html": "27888dcecfe89cc905aa6b067659e463",
+"/": "27888dcecfe89cc905aa6b067659e463",
 "main.dart.js": "0c8cc92124d4c69ad0398af663269461",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
